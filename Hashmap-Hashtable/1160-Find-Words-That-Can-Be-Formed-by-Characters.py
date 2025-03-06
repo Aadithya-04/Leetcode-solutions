ac91a643f99084417716566a733f213ffd388cc9
Aadithya E R Menon <menonaadithya2004@gmail.com>
2025-03-06 18:45:21 +0530

    solution added

---
+++ Hashmap-Hashtable/1160-Find-Words-That-Can-Be-Formed-by-Characters.py
@@ -0,0 +1,14 @@
+from collections import Counter
+from typing import List
+class Solution:
+    def countCharacters(self, words: List[str], chars: str) -> int:
+        char_count = Counter(chars)
+
+        total_length = 0
+
+        for word in words:
+            word_count = Counter(word)
+            if all(word_count[char]<=char_count[char] for char in word_count):
+                total_length += len(word)
+        return total_length 
+
