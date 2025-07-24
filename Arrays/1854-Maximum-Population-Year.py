2d689b9de74e947dea72b57a44ac664c3ba4ca97
Aadithya E R Menon <menonaadithya2004@gmail.com>
2025-07-24 23:01:50 +0530

    solution added

---
+++ Arrays/1854-Maximum-Population-Year.py
@@ -0,0 +1,20 @@
+class Solution:
+    def maximumPopulation(self, logs: List[List[int]]) -> int:
+        years = [0] * 101  # years from 1950 to 2050
+
+        # Track population change for each year
+        for birth, death in logs:
+            years[birth - 1950] += 1
+            years[death - 1950] -= 1
+
+        max_population = 0
+        max_year = 1950
+        current_population = 0
+
+        for i in range(101):
+            current_population += years[i]
+            if current_population > max_population:
+                max_population = current_population
+                max_year = 1950 + i
+
+        return max_year
